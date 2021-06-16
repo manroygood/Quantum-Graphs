48301bb4be7973a37fb8c0281d05cfe5e2728694
gconte23 <61422601+gconte23@users.noreply.github.com>
2021-06-16 15:54:40 -0400

    Updated qgDotCheb

--- source/@quantumGraph/qgdotCheb.m
+++ source/@quantumGraph/qgdotCheb.m
@@ -3,13 +3,15 @@ function z = qgdotCheb(Phi,u1col,u2col)
 % compatible with the template Phi. It just surges ahead and returns an
 % error if it doesn't work
 
-% u1 = copy(Phi); u1.column2graph(u1col);
-% u2 = copy(Phi); u2.column2graph(u2col);
-% weightVec = u1.Edges.Weight;
+%     u1 = copy(Phi); u1.column2graph(u1col);
+%     u2 = copy(Phi); u2.column2graph(u2col);
+%     weightVec = u1.Edges.Weight;
     weightVec = Phi.Edges.Weight;
+    [~,nxC,~] = Phi.nx;
+
     z = 0;
     for k = 1:numedges(Phi)
-        z = z + weightVec(k)*edgeDotCheb(Phi,u1col,u2col,k);
+        z = z + weightVec(k)*edgeDotCheb(Phi,u1col(nxC(k)+1:nxC(k+1)),u2col(nxC(k)+1:nxC(k+1)),k);
     end
 
 end 
