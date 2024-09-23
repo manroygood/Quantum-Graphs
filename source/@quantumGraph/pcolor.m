03ed6f10cf65f523ec225ec88c8f4045e87d4011
Roy Goodman <goodman@njit.edu>
2024-09-23 14:35:20 -0400

    a few little fixes for documentation and the paper

--- source/@quantumGraph/pcolor.m
+++ source/@quantumGraph/pcolor.m
@@ -16,19 +16,19 @@ for k=1:G.numedges
     colorplot2(x1,x2,y,uRange);
     hold on
 end
-% for k=1:G.numedges
-%     [y,x1,x2]=G.fullEdge(k);
-%     yS=y(1:end-1).*y(2:end);
-%         signChanges=find(yS<0); nsC=length(signChanges);
-%         if nsC>0
-%             s = y(signChanges)./(y(signChanges)-y(signChanges+1));
-%             x1s=x1(signChanges)+ s.*(x1(signChanges+1)-x1(signChanges));
-%             x2s=x2(signChanges)+ s.*(x2(signChanges+1)-x2(signChanges));
-%             plot(x1s,x2s,'ko','markerfacecolor','k')
-%         end
-% end
+for k=1:G.numedges
+    [y,x1,x2]=G.fullEdge(k);
+    yS=y(1:end-1).*y(2:end);
+        signChanges=find(yS<0); nsC=length(signChanges);
+        if nsC>0
+            s = y(signChanges)./(y(signChanges)-y(signChanges+1));
+            x1s=x1(signChanges)+ s.*(x1(signChanges+1)-x1(signChanges));
+            x2s=x2(signChanges)+ s.*(x2(signChanges+1)-x2(signChanges));
+            plot(x1s,x2s,'ko','markerfacecolor','k')
+        end
+end
 
 axis equal off
 view([0 90])
 
-colorbar
+cb=colorbar('southoutside')
